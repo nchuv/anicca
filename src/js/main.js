5924015d2ab237013c9080c466be9104b1259617
nchuv <natalya.chuvi@gmail.com>
2022-03-21 04:11:44 +0300

    magic cursor

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,19 @@
-console.log("OK - parcel-tailwind-starter");
+import LocomotiveScroll from 'locomotive-scroll';
+import { Cursor } from './cursor';
+
+const cursor = new Cursor(document.querySelector('.cursor'));
+
+// mouse effects on all links and others
+// [...document.querySelectorAll('a')].forEach(link => {
+//   link.addEventListener('mouseenter', () => cursor.enter());
+//   link.addEventListener('mouseleave', () => cursor.leave());
+// });
+
+// https://github.com/locomotivemtl/locomotive-scroll
+// https://locomotivemtl.github.io/locomotive-scroll/
+const lscroll = new LocomotiveScroll({
+  el: document.querySelector('[data-scroll-container]'),
+  smooth: true,
+  direction: 'up',
+});
+
